6fdd677edd17760cda73bdd71130e582fc0776f1
hanedamiki <haneda.miki@gmail.com>
2018-08-29 21:57:44 +0900

    by haneda

--- HP/HP/urls.py
+++ HP/HP/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('top/',include('top.urls')),
+    url(r'^contact/', include('contact.urls', namespace='member')),
 ]
